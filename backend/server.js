e1a82e851cfa2d3106fcc8a4c3f03da75bf9c153
OrestesJGR <orestesgarciarojo@hotmail.com>
2025-04-01 16:30:59 +0200

    Commit inicial TFG

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Conexión a MongoDB
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.use('/api', require('./routes/index'));
+
+// Inicio del servidor
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
